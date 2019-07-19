17ce05dea6876e41712a0cf269d5ae697dea2c27
Dan Visel <dbvisel@gmail.com>
2019-07-19 13:46:43 +0200

    put some content in

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,12 @@ const IndexPage = () => (
 			<h2>Welcome to Blurbsy</h2>
 			<p>description paragraph?</p>
 			<p>
-				(try starting <Link to="/author/lsasekoff">here</Link>.)
+				(try starting <Link to="/author/lsasekoff">here</Link> or <Link to="/book/camouflage">here</Link>.)
 			</p>
 			<p>(should this have a list of authors/books?)</p>
+		</section>
+		<section>
+			<h3>Submit a blurb:</h3>
 		</section>
 	</Layout>
 );
