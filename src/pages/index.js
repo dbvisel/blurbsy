ba86db32947652143b5f8d38bbe3173d99e7295e
Dan Visel <dbvisel@gmail.com>
2019-07-20 11:06:57 +0200

    added authorlist on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,70 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import styled from 'styled-components';
+import { graphql, StaticQuery, Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import cleanName from '../components/cleanname';
 
-const IndexPage = () => (
-	<Layout>
-		<SEO title="Blurbsy" />
-		<section>
-			<h2>Welcome to Blurbsy</h2>
-			<p>description paragraph?</p>
-			<p>
-				(try starting <Link to="/author/lsasekoff">here</Link> or <Link to="/book/camouflage">here</Link>.)
-			</p>
-			<p>(should this have a list of authors/books?)</p>
-		</section>
-		<section>
-			<h3>Submit a blurb:</h3>
-		</section>
-	</Layout>
-);
+const AuthorName = styled.li`
+	display: inline-block;
+	&:after {
+		content: '·';
+		margin-left: 0.5em;
+		margin-right: 0.5em;
+	}
+	&:last-of-type {
+		&:after {
+			content: '';
+			margin: 0;
+		}
+	}
+`;
+
+const IndexPage = () => {
+	return (
+		<StaticQuery
+			query={graphql`
+				query AuthorList {
+					allAirtable(filter: { table: { eq: "authors" } }) {
+						edges {
+							node {
+								data {
+									Name
+								}
+							}
+						}
+					}
+				}
+			`}
+			render={data => {
+				console.log(data);
+				return (
+					<Layout>
+						<SEO title="Blurbsy" />
+						<section>
+							<h2>Welcome to Blurbsy</h2>
+							<p>description paragraph?</p>
+						</section>
+						<section>
+							<h2>Authors:</h2>
+							<ul>
+								{data.allAirtable.edges.map((node, index) => (
+									<AuthorName key={index}>
+										<Link to={`/author/${cleanName(node.node.data.Name)}`}>{node.node.data.Name}</Link>
+									</AuthorName>
+								))}
+							</ul>
+						</section>
+						<section>
+							<h3>Submit a blurb:</h3>
+						</section>
+					</Layout>
+				);
+			}}
+		/>
+	);
+};
 
 export default IndexPage;
 
